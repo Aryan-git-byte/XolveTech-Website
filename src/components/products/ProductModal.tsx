e8a36bacb997b6ba5f1d746649c17ab988f4f84b
Aryan Kumar <aryan17550@gmail.com>
2025-08-09 07:46:08 +0530

    Updated ProductModal.tsx

--- src/components/products/ProductModal.tsx
+++ src/components/products/ProductModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Product } from '../../types'
 import { Modal } from '../ui/Modal'
 import { Button } from '../ui/Button'

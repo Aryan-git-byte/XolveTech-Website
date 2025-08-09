4fd109d5abaf6925d42411e82c34d355f2355ce0
Aryan Kumar <aryan17550@gmail.com>
2025-08-09 07:45:33 +0530

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Menu, X, Settings, ShoppingCart, User, LogOut } from 'lucide-react'
 import { useAuth } from '../../contexts/AuthContext'
